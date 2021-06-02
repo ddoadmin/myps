f7f94e44f324f86a2c332c7bba0fed44f3d0df93
ddoadmin <deepakd_fic@yahoo.com>
2021-06-02 11:05:21 -0500

    Update findvgaram.ps1

--- findvgaram.ps1
+++ findvgaram.ps1
@@ -1,1 +1,2 @@
+## Update following %Intel% with name of your video card
 Get-WmiObject Win32_VideoController -filter "name like '%Intel%'" | select AdapterRAM,@{Expression={$_.adapterram/1MB};label="MB"}
